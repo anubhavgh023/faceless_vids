ad8a4e7840819d785181112be2928884f0d3bf83
Anubhav <anubhavsamanta023@gmail.com>
2024-10-13 09:44:23 +0530

    aspect_ratio passing from main

--- create_video/main.py
+++ create_video/main.py
@@ -1,12 +1,6 @@
 from image_processing import make_video_from_image
-from audio_processing import (
-    merge_audios,
-    generate_word_timings
-)
-from subtitle_processing import (
-    generate_subtitle_file, 
-    modify_subtitle_style
-)
+from audio_processing import merge_audios, generate_word_timings
+from subtitle_processing import generate_subtitle_file, modify_subtitle_style
 from video_processing import (
     merge_videos,
     stabilize_video,
@@ -14,21 +8,28 @@ from video_processing import (
     add_bg_music,
 )
 
+
 # Main script to process multiple prompts
 if __name__ == "__main__":
-    images = [f"assets/images/t4_story_img_{i}.png" for i in range(1, 6)]
-    audios = [f"assets/audio/prompt_{i}.mp3" for i in range(1, 6)]
     captions = []
     # Read prompts again to use as captions
     with open("../prompts/prompts.txt", "r") as f:
         captions = f.read().splitlines()
 
+    # segments will vary according to the no. of images
+    # no. of images = no. of caption
+    segments = len(captions)
+
+    images = [f"assets/images/story_img_{i+1}.png" for i in range(segments)]
+    audios = [f"assets/audio/prompt_{i+1}.mp3" for i in range(segments)]
+
     processed_videos = []
 
     # Step 1: Generate videos for each image (without audio)
-    for i in range(5):
-        output_video = f"assets/videos/t4_story_video_{i + 1}.mp4"
-        make_video_from_image(images[i], output_video)
+    for i in range(segments):
+        output_video = f"assets/videos/story_video_{i + 1}.mp4"
+        aspect_ratio = "1024:1729"  # modify aspect ratio
+        make_video_from_image(images[i], output_video, aspect_ratio)
         processed_videos.append(output_video)
 
     # Step 2: Merge all generated videos
@@ -65,9 +66,7 @@ if __name__ == "__main__":
 
     # Step 9: Add background music to the final video
     final_output_video = "assets/videos/final_output_video.mp4"
-    bg_music_path = (
-        "assets/bg_music/bg_1.mp3"
-    )
+    bg_music_path = "assets/bg_music/bg_2.mp3"
     add_bg_music(final_video_with_subtitles, bg_music_path, final_output_video)
 
     print("Video processing completed successfully!")
